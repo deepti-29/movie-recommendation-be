0421ebadfc446b5d10d131195e4e71c1af6a2de5
Deepti <s.deepti2906@gmail.com>
2022-05-29 17:03:57 +0530

    added flask-corse

--- wsgi.py
+++ wsgi.py
@@ -1,28 +1,26 @@
-import json
 import os
-
 import flask
 from flask import Flask, request, jsonify
-
 import util
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 model, mat_movies = util.pre_process()
 
 
 @app.route("/")
+@cross_origin()
 def welcome():
     return flask.render_template('welcome.html')
 
 
 @app.route("/movie/<movie>")
+@cross_origin()
 def recommend_movie(movie):
     args = request.args
     output = util.recommender(movie, mat_movies, model, args.get('count', default=10, type=int))
-    response = jsonify(success=True, message="Data Retrieved Successfully.", data=output), 201
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+    return jsonify(success=True, message="Data Retrieved Successfully.", data=output), 201
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
