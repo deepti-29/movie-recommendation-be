fc378d3915a12c2ca1bb423cc7888190fb419762
Deepti <s.deepti2906@gmail.com>
2022-05-29 16:28:48 +0530

    Added header for enabling cors

--- wsgi.py
+++ wsgi.py
@@ -20,8 +20,9 @@ def welcome():
 def recommend_movie(movie):
     args = request.args
     output = util.recommender(movie, mat_movies, model, args.get('count', default=10, type=int))
-    return jsonify(success=True, message="Data Retrieved Successfully.", data=output), 201
-
+    response = jsonify(success=True, message="Data Retrieved Successfully.", data=output), 201
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
